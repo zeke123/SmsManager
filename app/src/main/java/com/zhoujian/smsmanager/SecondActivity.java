1f9c9ba1337e6e8c3018c1f8ae7ce517beac1634
zhoujian <1064791247@qq.com>
2016-11-11 16:35:05 +0800

    zeke  短信群发

---
+++ app/src/main/java/com/zhoujian/smsmanager/SecondActivity.java
@@ -0,0 +1,50 @@
+package com.zhoujian.smsmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhoujian on 2016/11/11.
+ *
+ * 短信群发的界面
+ */
+
+public class SecondActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
